ce3cc4e3d5eb7fe4f9a1377adbc948fa5d053eba
ArmandoArV <armando.arredondo.valle@gmail.com>
2022-07-26 18:54:42 -0500

    MODIFICATIONS IN THE JS AND CSS

--- js/classPokemon.js
+++ js/classPokemon.js
@@ -4,8 +4,8 @@
     */
         let title = "";
 
-        const types = ["Plant", "Fire", "Water", "Earth", "Electric"];
-        const ids = ["plant_pokemons", "fire_pokemons", "water_pokemons", "earth_pokemons", "electric_pokemons"];
+        const types = ["Plant", "Fire", "Water", "Ground", "Electric"];
+        const ids = ["plant_pokemons", "fire_pokemons", "water_pokemons", "ground_pokemons", "electric_pokemons"];
         class Pokemon{
             constructor(type, name, hp, attack, defense, speed){
                 this.type = type;
@@ -74,15 +74,21 @@
         var pokemons = [];
         pokemons.push(new Pokemon(types[0], "Bulbasaur", 45, 49, 49, 45, 65));
         pokemons.push(new Pokemon(types[0], "Ivysaur", 60, 62, 63, 60, 80));
+        pokemons.push(new Pokemon(types[0], "Venusaur", 80, 82, 83, 80, 100));
         pokemons.push(new Pokemon(types[1], "Charmander", 39, 52, 43, 65, 65));
         pokemons.push(new Pokemon(types[2], "Squirtle", 44, 48, 65, 65, 43));
+        pokemons.push(new Pokemon(types[2], "Wartortle", 59, 63, 80, 80, 58));
+        pokemons.push(new Pokemon(types[1], "Charmeleon", 58, 44, 58, 80, 65));
+        pokemons.push(new Pokemon(types[1], "Charizard", 78, 84, 78, 109, 85));
+        pokemons.push(new Pokemon(types[2], "Blastoise", 79, 83, 100, 85, 78));
         pokemons.push(new Pokemon(types[3], "Diglett", 43, 65, 65, 48, 43));
         pokemons.push(new Pokemon(types[4], "Pikachu", 35, 55, 40, 50, 90));
-        // Function for check if the type is in the types array
-    
+        pokemons.push(new Pokemon(types[4], "Raichu", 60, 90, 55, 90, 110));
+        pokemons.push(new Pokemon(types[3], "Sandshrew", 50, 75, 85, 40, 30));
+        pokemons.push(new Pokemon(types[3], "Sandslash", 75, 100, 110, 65, 55));
+        pokemons.push(new Pokemon(types[4], "Nidoran", 55, 47, 52, 40, 41));
+        
 
-
-        // Function for check if the type is in the types array
         function checkImage(name){
             for(var i = 0; i < pokemons.length; i++){
                 if(name == pokemons[i].getName()){
@@ -90,8 +96,7 @@
                 }
             }
         }
-        
-    
+
         for(var i = 0; i < pokemons.length; i++){
             var type = pokemons[i].getType();
             for(var j = 0; j < types.length; j++){
@@ -112,9 +117,9 @@
                     let textNode3 = document.createTextNode("Attack: "+pokemons[i].getAttack() + " ");
                     let textNode4 = document.createTextNode("Defense: "+pokemons[i].getDefense() + " ");
                     let textNode5 = document.createTextNode("Speed: "+pokemons[i].getSpeed() + " ");
-
                     img.setAttribute("src", "images/" + pokemons[i].getName() + ".png");
                     img.setAttribute("alt", pokemons[i].getName());
+                    img.setAttribute("id", "PokemonImage");
                     h3.appendChild(textNode);
                     p.appendChild(textNode2);
                     p.appendChild(textNode3);
